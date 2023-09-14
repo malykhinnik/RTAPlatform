d9131a82fbc367978cea148691c1f67a7ac28c70
Nikolay Malykhin <malikhinspb@gmail.com>
2023-09-14 18:08:02 +0300

    cleanup

--- backend/src/main/java/com/rtaplatform/kafka/KafkaUtils.java
+++ backend/src/main/java/com/rtaplatform/kafka/KafkaUtils.java
@@ -4,7 +4,8 @@ import org.springframework.util.StringUtils;
 
 public class KafkaUtils {
     public static final String MESSAGE_VALIDATION_REGEX = "[\\s*|\\S\\d\\w]*\"uuid\"\\s*:\\s*\"[0-9a-fA-F]{8}\\b-[0-9a-fA-F]{4}\\b-[0-9a-fA-F]{4}\\b-[0-9a-fA-F]{4}\\b-[0-9a-fA-F]{12}\"[\\s*|\\S\\d\\w]*";
-    public static  boolean isValidMessage(String message) {
+
+    public static boolean isValidMessage(String message) {
         return StringUtils.hasLength(message) &&
                 message.matches(MESSAGE_VALIDATION_REGEX);
     }
--- backend/src/test/java/com/rtaplatform/kafka/KafkaUtilsTest.java
+++ backend/src/test/java/com/rtaplatform/kafka/KafkaUtilsTest.java
@@ -4,7 +4,8 @@ import org.junit.jupiter.api.Test;
 
 import static com.rtaplatform.kafka.KafkaTestConstants.KAFKA_VALID_MESSAGE;
 import static com.rtaplatform.kafka.KafkaUtils.isValidMessage;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class KafkaUtilsTest {
