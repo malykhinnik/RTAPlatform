e2df3af079e55d199c1cc1cd90daea74f71c0e87
Nikolay Malykhin <malikhinspb@gmail.com>
2023-09-14 14:59:47 +0300

    clean module

--- backend/target/test-classes/db/migration/V1__create_app_user_table.sql
+++
@@ -1,8 +0,0 @@
-create table app_user
-(
-    id          bigserial not null unique primary key,
-    created     timestamp with time zone,
-    updated     timestamp with time zone,
-    first_name  varchar(256),
-    last_name   varchar(256)
-);
